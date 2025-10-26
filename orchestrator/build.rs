e77763744097a68872911361482aaca7e57b5f4b
Kyugito <113055222+Kyugito666@users.noreply.github.com>
2025-10-26 22:54:00 +0700

    Update build.rs

--- orchestrator/build.rs
+++ orchestrator/build.rs
@@ -1,27 +1,42 @@
-// build.rs - C++ compilation for libsodium crypto
-
+// build.rs - Improved version
 use std::env;
 use std::path::PathBuf;
 
 fn main() {
+    println!("cargo:rerun-if-changed=cpp/crypto.cpp");
+    println!("cargo:rerun-if-changed=cpp/crypto.h");
+    
+    // Try to link libsodium
+    println!("cargo:rustc-link-lib=sodium");
+    
     let mut build = cc::Build::new();
     
     build
         .cpp(true)
         .file("cpp/crypto.cpp")
-        .flag("-std=c++17")
-        .flag("-lsodium");
+        .flag_if_supported("-std=c++17")
+        .warnings(false);
     
     // Platform-specific flags
     if cfg!(target_os = "windows") {
-        build.flag("/EHsc");
+        build.flag_if_supported("/EHsc");
     } else {
-        build.flag("-fPIC");
+        build.flag_if_supported("-fPIC");
+    }
+    
+    // Try to find libsodium
+    if let Ok(include_path) = env::var("SODIUM_INCLUDE_DIR") {
+        build.include(include_path);
     }
     
     build.compile("nexus_crypto");
     
-    println!("cargo:rerun-if-changed=cpp/crypto.cpp");
-    println!("cargo:rerun-if-changed=cpp/crypto.h");
-    println!("cargo:rustc-link-lib=sodium");
+    // Print helpful message
+    if cfg!(target_os = "linux") {
+        println!("cargo:warning=Make sure libsodium-dev is installed: sudo apt-get install libsodium-dev");
+    } else if cfg!(target_os = "macos") {
+        println!("cargo:warning=Make sure libsodium is installed: brew install libsodium");
+    } else if cfg!(target_os = "windows") {
+        println!("cargo:warning=Make sure libsodium is installed. See: https://doc.libsodium.org/installation");
+    }
 }
